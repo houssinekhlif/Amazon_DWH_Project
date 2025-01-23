3ceef9098f9f3de825d0772718b1af4c1bad2e28
Mouna-Saadaoui <158584732+Mouna-Saadaoui@users.noreply.github.com>
2025-01-23 19:11:43 +0100

    Add files via upload

---
+++ Datawarehouse data files/Demographics_Dimension.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Demographics_Dimension ([Region] TEXT, [Population] TEXT, [Median_Income] TEXT, [Unemployment_Rate] TEXT, [Competitor_Count] TEXT, [Growth_Rate] TEXT);
+INSERT INTO Demographics_Dimension VALUES ('North America', '573042', '41771', '7.7', '45', '1.6');
+INSERT INTO Demographics_Dimension VALUES ('South America', '1253877', '39442', '6.2', '19', '3.1');
+INSERT INTO Demographics_Dimension VALUES ('Europe', '445806', '49285', '5.7', '22', '3.1');
+INSERT INTO Demographics_Dimension VALUES ('Africa', '414066', '61536', '9.4', '47', '6.9');
+INSERT INTO Demographics_Dimension VALUES ('Asia', '443183', '69877', '3.2', '12', '5.6');
+INSERT INTO Demographics_Dimension VALUES ('Oceania', '882492', '54848', '5.5', '48', '5.1');
+INSERT INTO Demographics_Dimension VALUES ('Middle East', '1335146', '54111', '5.6', '26', '6.1');
