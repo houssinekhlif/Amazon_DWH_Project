71ecd5f2396f63deff21935ce733da198cbebfbb
houssinekhlif <158588470+houssinekhlif@users.noreply.github.com>
2025-01-24 12:00:39 +0100

    Add files via upload

---
+++ Datawarehouse Data Files/Geography_Dimension.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Geography_Dimension ([Region] TEXT, [Latitude] TEXT, [Longitude] TEXT, [Current_Store_Count] TEXT, [Potential_New_Stores] TEXT, [Market_Size] TEXT);
+INSERT INTO Geography_Dimension VALUES ('North America', 79.2904, 115.3329, 14, 7, 896502);
+INSERT INTO Geography_Dimension VALUES ('South America', 9.6735, 149.3287, 18, 9, 820428);
+INSERT INTO Geography_Dimension VALUES ('Europe', 75.5292, 32.5999, 14, 1, 967973);
+INSERT INTO Geography_Dimension VALUES ('Africa', -28.3753, -113.8712, 9, 6, 398584);
+INSERT INTO Geography_Dimension VALUES ('Asia', 51.6547, -122.3944, 6, 5, 953840);
+INSERT INTO Geography_Dimension VALUES ('Oceania', 0.7682, -115.5525, 21, 4, 770658);
+INSERT INTO Geography_Dimension VALUES ('Middle East', 62.6007, -112.5801, 8, 9, 515488);
